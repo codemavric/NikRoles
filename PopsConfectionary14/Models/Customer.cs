e8712822cb2dc9a89795538f25b37ffdcd476dd8
Vicram RJ Purshotam <vicram.purshotam@gmail.com>
2017-09-15 17:17:44 +0200

    Add project files.

---
+++ PopsConfectionary14/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PopsConfectionary14.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string ContactNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Date Of Birth")]
+        public DateTime BD { get; set; }
+    }
+}
